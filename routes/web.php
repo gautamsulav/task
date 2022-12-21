20a5499f675c6fc524ceef09de34603617f4fe41
Sulav Sharma Gautam <sulav@sulavs-mbp.lan>
2022-12-21 12:28:59 -0600

    remove unsued routes from web routes

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,3 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::apiResource('users', UserController::class);
-
-
-Route::get('/', function () {
-    return view('welcome');
-});
